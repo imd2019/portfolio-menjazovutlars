b092fb7f96d80fd55c7ab0155721f532b0cbd4e0
larsibarsi <58939896+menjazovutlars@users.noreply.github.com>
2020-05-29 20:27:33 +0200

    bug fixing

--- yahtzee/p5setup.js
+++ yahtzee/p5setup.js
@@ -3,13 +3,13 @@ let width = windowWidth;
 let height = windowHeight;
 
 function setup() {
-  let s = sketch.createCanvas(windowWidth / 3, windowHeight / 1.2);
+  let s = sketch.createCanvas(windowWidth / 3, windowHeight / 1.5);
   sketch.frameRate(30);
   s.parent("sketch-holder");
 }
 window.setup = setup;
 
 function windowResized() {
-  sketch.resizeCanvas(windowWidth / 3, windowHeight / 1.2);
+  sketch.resizeCanvas(windowWidth / 3, windowHeight / 1.5);
 }
 window.addEventListener("resize", windowResized);
